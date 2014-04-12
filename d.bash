29c36c9b1ff9ed096ac95032c10a6ccea1a03da6
PC9 <PapaCharlie9@gmail.com>
2014-04-12 10:44:53 -0700

    initial commit

---
+++ d.bash
@@ -0,0 +1,4 @@
+echo cp -f CVoteBan.cs ~/procon/Plugins/BF3/MULTIbalancer.cs 
+cp -f CVoteBan.cs ~/procon/Plugins/BF3/MULTIbalancer.cs
+echo cp -f CVoteBan.cs ~/procon/Plugins/BF4/MULTIbalancer.cs 
+cp -f CVoteBan.cs ~/procon/Plugins/BF4/MULTIbalancer.cs
